8cd53e439e71f08f801206bac1631f76192f88fd
Djmax23 <114903050+Djmax23@users.noreply.github.com>
2024-10-13 20:45:48 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,47 @@
+var vg_1 = "index-map.vg";
+var vg_2 = "el-nino-chart.vg";
+var vg_3 = "el-nino-donut.vg";
+var vg_4 = "el-nino-map.vg";
+var vg_5 = "el-nino-temp-chart.vg";
+
+var vg_6 = "la-nina-chart.vg";
+var vg_7 = "la-nina-donut.vg";
+var vg_8 = "la-nina-map.vg";
+var vg_9 = "la-nina-temp-chart.vg";
+
+
+vegaEmbed("#index-map", vg_1).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#el-nino-chart", vg_2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#el-nino-donut", vg_3).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#el-nino-map", vg_4).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#el-nino-temp-chart", vg_5).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#la-nina-chart", vg_6).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#la-nina-donut", vg_7).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#la-nina-map", vg_8).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#la-nina-temp-chart", vg_9).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
